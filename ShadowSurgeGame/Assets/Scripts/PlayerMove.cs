ac951aa504adf17755892ffc91121cb3087bcb9d
fodedoucara <fodedoucara2003@gmail.com>
2025-08-01 19:03:53 -0500

    ai model

---
+++ ShadowSurgeGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float movement_speed = 3f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
