19b88f9f280117872210b7014b9522bb9de8d51d
fodedoucara <fodedoucara2003@gmail.com>
2025-08-01 20:26:12 -0500

    player cant still move but can be debugged

--- ShadowSurgeGame/Assets/Scripts/PlayerMove.cs
+++ ShadowSurgeGame/Assets/Scripts/PlayerMove.cs
@@ -1,17 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
 
 public class PlayerMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float movement_speed = 3f;
+    public float gravity = 9.8f;
+    public float rotation_Speed = 0.15f;
+    public float rotateDegreesPerSecond = 180f;
+    private CharacterController charController;
+
+    void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+    void Move()
+    {
+        print("value = " + Input.GetAxis("Vertical"));
+        if (Input.GetAxis("Vertical") > 0)
+        {
+
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+
+        }
     }
 }
